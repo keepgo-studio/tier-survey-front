f07c74843aa6bb17c5d5465861841dbd504dcf65
keepgo-studio <keepgo.studio@gmail.com>
2024-03-22 10:58:09 +0900

    Fix: dev env comparing

--- src/e_shared/utils/vars.ts
+++ src/e_shared/utils/vars.ts
@@ -1,8 +1,8 @@
-export const NEXT_API_URL = process.env.NODE_ENV
+export const NEXT_API_URL = process.env.NODE_ENV === 'development'
   ? "http://localhost:3000"
   : "https://tier-survey.xyz";
 
-export const FB_API_URL = process.env.NODE_ENV
+export const FB_API_URL = process.env.NODE_ENV === 'development'
   ? "http://127.0.0.1:5001/tier-survey/asia-northeast3"
   : "https://asia-northeast3-tier-survey.cloudfunctions.net";
 
